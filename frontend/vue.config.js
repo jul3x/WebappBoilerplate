2ca25aa610bb36a3fdecbf1c1f6e984b91b2616c
jul3x <jprolejko@gmail.com>
2024-10-16 22:50:55 +0200

    Modify page title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'WebApp Template',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+    }
 });
